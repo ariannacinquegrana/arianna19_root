ecef729416c6eb4194213a056798c9535c829a99
ariannacinquegrana <2477619@edu.vaniercollege.qc.ca>
2025-03-04 12:29:19 -0500

    Capython board game web

---
+++ public_html/web2/capython/pathfinder.js
@@ -0,0 +1,8 @@
+
+const panel = document.getElementById("panel");
+    const sound = new Audio("sounds/door.mp3");
+
+    panel.onclick = function() {
+         this.classList.toggle("slide-up");
+         sound.play();
+    }
